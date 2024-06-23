16affea0b1525e0673a8199b392dbfad1df82f18
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2024-06-23 17:30:28 +0200

    internal state with mutex and lazy static

--- logic_component/src/lib.rs
+++ logic_component/src/lib.rs
@@ -1,5 +1,9 @@
 mod return_type;
+
+use std::collections::HashMap;
 use return_type::ReturnType;
+use std::sync::Mutex;
+
 
 #[no_mangle]
 pub extern "C" fn add(left: usize, right: usize) -> usize {
@@ -16,6 +20,59 @@ pub extern "C" fn return_array() -> *const ReturnType {
     };
     Box::into_raw(Box::new(return_type))
 }
+
+
+lazy_static::lazy_static! {
+    static ref STATE: Mutex<State> = Mutex::new(State::new());
+}
+
+#[derive(Debug)]
+struct State {
+    counter: i32,
+    some_keys: HashMap<String, i32>
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            counter: 0,
+            some_keys: HashMap::new()
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn increment_counter() -> i32 {
+    let mut state = STATE.lock().unwrap();
+    state.counter += 1;
+    state.counter
+}
+
+#[no_mangle]
+pub extern "C" fn get_counter() -> i32 {
+    let state = STATE.lock().unwrap();
+    state.counter
+}
+
+#[no_mangle]
+pub extern "C" fn set_key(key: *const u8, key_len: usize, value: i32) {
+    let key = unsafe { std::slice::from_raw_parts(key, key_len) };
+    let key = String::from_utf8_lossy(key).to_string();
+    let mut state = STATE.lock().unwrap();
+    state.some_keys.insert(key, value);
+}
+
+#[no_mangle]
+pub extern "C" fn get_value(key: *const u8, key_len: usize) -> i32 {
+    let key = unsafe { std::slice::from_raw_parts(key, key_len) };
+    let key = String::from_utf8_lossy(key).to_string();
+    let state = STATE.lock().unwrap();
+    match state.some_keys.get(&key) {
+        Some(value) => *value,
+        None => 0
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ctypes
+import sys
 from ctypes import POINTER, Structure, c_int, c_size_t, c_void_p
 
 # Define the ReturnType structure in Python
@@ -9,6 +10,7 @@ class ReturnType(Structure):
     ]
 
 
+# Load the shared library
 lib = ctypes.CDLL('./target/debug/logic_component.dll')
 
 # Define the return type of the return_array function
@@ -25,3 +27,17 @@ return_type = return_type_ptr.contents
 # Access the fields of the struct
 print(f"Array: {list(return_type.array)}")
 print(f"Tuple: ({return_type.tuple[0]}, {return_type.tuple[1]})")
+
+print(f"Counter: {lib.get_counter()}")
+print("Incrementing counter")
+lib.increment_counter()
+print(f"Counter: {lib.get_counter()}")
+
+print("Setting a key-value pair")
+key = b"some key"
+lib.set_key(key, sys.getsizeof(key), 42)
+value = lib.get_value(key, sys.getsizeof(key))
+print(f"Value: {value}")
+
+other_key = b"other key"
+print(f"Non-existent value: {lib.get_value(other_key, sys.getsizeof(other_key))}")
